34dcc631234007efc82ee2f4475709abf29d6ee2
RubyScarIet <144369755+RubyScarIet@users.noreply.github.com>
2026-03-28 02:59:07 +0700

    Delete .history/SourceCode/scripts directory

--- .history/SourceCode/scripts/Test_20260316212027.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = "C:/Users/tinca/OneDrive/Desktop/Identifying and classifying book covers using YOLO/SourceCode/dataset"
-if os.exists(path):
-    print("✅ Đường dẫn chính xác!")
-    print(f"Danh sách thư mục con: {os.listdir(path)}")
-else:
-    print("❌ Không tìm thấy đường dẫn. Hãy kiểm tra lại!")
